a788b30b6bafcfe08d8ca5c021325074223683a7
Calvin Shin <calvin.shin@gmail.com>
2019-06-06 23:25:05 -0700

    Used console.table to return the items in the sql server

--- assets/js/customer/customerWelcome.js
+++ assets/js/customer/customerWelcome.js
@@ -1,23 +1,17 @@
 var inquirer = require("inquirer");
-var sqlConnection = require("../assets/sqlConnection")
+var buyItem = require("./buyItem")
 
 var customerWelcome = function() {
     inquirer.prompt([
         {
             name: "custOption",
-            prompt: "What would you like to do today?",
+            message: "What would you like to do today?",
             choices: ["Buy an item"],
             type: "list",
         }
     ]).then(function(response) {
         if(response.custOption === "Buy an item") {
-            console.log("Great, let's buy things!");
-
-            sqlConnection.query("SELECT * FROM accounts", function(err, res) {
-                if (err) throw err;
-                console.log(res);
-            });
-
+            buyItem();
         }
     })
 };
